0f93982940573c8e2a130bd9aa0af1fcb4257788
wlm2012 <541253785@qq.com>
2023-06-14 23:24:55 +0800

    AbstractAutowireCapableBeanFactory

--- src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -1,5 +1,29 @@
 package org.springframework.beans.factory.support;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+
+import java.lang.reflect.InvocationTargetException;
+
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
-    
+
+    @Override
+    protected Object createBean(String beanName, BeanDefinition beanDefinition) {
+        return doCreateBean(beanName, beanDefinition);
+    }
+
+    protected Object doCreateBean(String beanName, BeanDefinition beanDefinition) {
+        Class beanClass = beanDefinition.getBeanClass();
+        Object bean;
+        try {
+
+            bean = beanClass.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            throw new BeansException("Instantiation of bean failed", e);
+        }
+
+        addSingleton(beanName, bean);
+        return bean;
+    }
+
 }
