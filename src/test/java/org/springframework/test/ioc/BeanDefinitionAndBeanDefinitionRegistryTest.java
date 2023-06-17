fc19e87882b754b8a6b2dfc973cd475d5df56ec3
wlm2012 <541253785@qq.com>
2023-06-17 17:16:16 +0800

    DefaultListableBeanFactory

---
+++ src/test/java/org/springframework/test/ioc/BeanDefinitionAndBeanDefinitionRegistryTest.java
@@ -0,0 +1,18 @@
+package org.springframework.test.ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+
+public class BeanDefinitionAndBeanDefinitionRegistryTest {
+
+	@Test
+	public void testBeanFactory() {
+		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+		BeanDefinition beanDefinition = new BeanDefinition(HelloService.class);
+		beanFactory.registerBeanDefinition("helloService", beanDefinition);
+
+		HelloService helloService = (HelloService) beanFactory.getBean("helloService");
+		helloService.sayHello();
+	}
+}
