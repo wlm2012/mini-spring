6680e3a9d1a8aca3522dc01b53b78d146cf58cc4
wlm2012 <541253785@qq.com>
2023-06-24 18:42:36 +0800

    testPopulateBeanWithBean

--- src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -4,6 +4,7 @@ import cn.hutool.core.bean.BeanUtil;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.PropertyValue;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanReference;
 
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory {
 
@@ -43,7 +44,9 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
             for (PropertyValue propertyValue : beanDefinition.getPropertyValues().getPropertyValues()) {
                 String name = propertyValue.getName();
                 Object value = propertyValue.getValue();
-
+                if (value instanceof BeanReference beanReference) {
+                    value = getBean(beanReference.getBeanName());
+                }
                 //通过反射设置属性
                 BeanUtil.setFieldValue(bean, name, value);
             }
--- src/test/java/org/springframework/test/ioc/PopulateBeanWithPropertyValuesTest.java
+++ src/test/java/org/springframework/test/ioc/PopulateBeanWithPropertyValuesTest.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.PropertyValue;
 import org.springframework.beans.PropertyValues;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanReference;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.test.ioc.bean.Car;
 import org.springframework.test.ioc.bean.Person;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PopulateBeanWithPropertyValuesTest {
@@ -24,5 +27,33 @@ public class PopulateBeanWithPropertyValuesTest {
         System.out.println(person);
         assertEquals("derek", person.getName());
         assertEquals(18, person.getAge());
+    }
+
+    @Test
+    public void testPopulateBeanWithBean() throws Exception {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        //注册Car实例
+        PropertyValues propertyValuesForCar = new PropertyValues();
+        propertyValuesForCar.addPropertyValue(new PropertyValue("brand", "porsche"));
+        BeanDefinition carBeanDefinition = new BeanDefinition(Car.class, propertyValuesForCar);
+        beanFactory.registerBeanDefinition("car", carBeanDefinition);
+
+        //注册Person实例
+        PropertyValues propertyValuesForPerson = new PropertyValues();
+        propertyValuesForPerson.addPropertyValue(new PropertyValue("name", "derek"));
+        propertyValuesForPerson.addPropertyValue(new PropertyValue("age", 18));
+        //Person实例依赖Car实例
+        propertyValuesForPerson.addPropertyValue(new PropertyValue("car", new BeanReference("car")));
+        BeanDefinition beanDefinition = new BeanDefinition(Person.class, propertyValuesForPerson);
+        beanFactory.registerBeanDefinition("person", beanDefinition);
+
+        Person person = (Person) beanFactory.getBean("person");
+        System.out.println(person);
+        assertThat(person.getName()).isEqualTo("derek");
+        assertThat(person.getAge()).isEqualTo(18);
+        Car car = person.getCar();
+        assertThat(car).isNotNull();
+        assertThat(car.getBrand()).isEqualTo("porsche");
     }
 }
