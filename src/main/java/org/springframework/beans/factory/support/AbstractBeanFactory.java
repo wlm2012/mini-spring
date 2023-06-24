d160b55131e55df525d40f97d70c045b8a57c712
wlm2012 <541253785@qq.com>
2023-06-24 21:57:57 +0800

    AbstractBeanFactory

--- src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
@@ -1,9 +1,9 @@
 package org.springframework.beans.factory.support;
 
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 
-public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry implements BeanFactory {
+public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry implements ConfigurableBeanFactory {
 
     @Override
     public Object getBean(String name) {
