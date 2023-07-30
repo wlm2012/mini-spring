701c0ed5aee5f1523efeb0c164060bb053f9ac5b
wlm2012 <541253785@qq.com>
2023-07-30 16:34:20 +0800

    testCglibDynamicProxy

--- src/test/java/org/springframework/test/aop/DynamicProxyTest.java
+++ src/test/java/org/springframework/test/aop/DynamicProxyTest.java
@@ -1,10 +1,13 @@
 package org.springframework.test.aop;
 
+import cn.hutool.aop.proxy.ProxyFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.AdvisedSupport;
 import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.TargetSource;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.framework.CglibAopProxy;
 import org.springframework.aop.framework.JdkDynamicAopProxy;
 import org.springframework.test.common.WorldServiceInterceptor;
 import org.springframework.test.service.WorldService;
@@ -12,21 +15,45 @@ import org.springframework.test.service.WorldServiceImpl;
 
 public class DynamicProxyTest {
 
-	@Test
-	public void testJdkDynamicProxy() {
+	private AdvisedSupport advisedSupport;
+
+	@BeforeEach
+	public void setup() {
 		WorldService worldService = new WorldServiceImpl();
 
-
+		advisedSupport = new AdvisedSupport();
 		TargetSource targetSource = new TargetSource(worldService);
 		WorldServiceInterceptor methodInterceptor = new WorldServiceInterceptor();
 		MethodMatcher methodMatcher = new AspectJExpressionPointcut("execution(* org.springframework.test.service.WorldService.explode(..))").getMethodMatcher();
-
-		AdvisedSupport advisedSupport = new AdvisedSupport();
 		advisedSupport.setTargetSource(targetSource);
 		advisedSupport.setMethodInterceptor(methodInterceptor);
 		advisedSupport.setMethodMatcher(methodMatcher);
+	}
 
+	@Test
+	public void testJdkDynamicProxy() throws Exception {
 		WorldService proxy = (WorldService) new JdkDynamicAopProxy(advisedSupport).getProxy();
+		proxy.explode();
+	}
+
+
+	@Test
+	public void testCglibDynamicProxy() throws Exception {
+		WorldService proxy = (WorldService) new CglibAopProxy(advisedSupport).getProxy();
+		proxy.explode();
+	}
+
+
+	@Test
+	public void testProxyFactory() throws Exception {
+		// 使用JDK动态代理
+		advisedSupport.setProxyTargetClass(false);
+		WorldService proxy = (WorldService) new ProxyFactory(advisedSupport).getProxy();
+		proxy.explode();
+
+		// 使用CGLIB动态代理
+		advisedSupport.setProxyTargetClass(true);
+		proxy = (WorldService) new ProxyFactory(advisedSupport).getProxy();
 		proxy.explode();
 	}
 }
