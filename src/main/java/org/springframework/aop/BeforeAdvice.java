6b675aa5f94f5800ec0c9b38d6838539e52438b4
wlm2012 <541253785@qq.com>
2023-07-30 16:47:41 +0800

    testBeforeAdvice

---
+++ src/main/java/org/springframework/aop/BeforeAdvice.java
@@ -0,0 +1,7 @@
+package org.springframework.aop;
+
+import org.aopalliance.aop.Advice;
+
+public interface BeforeAdvice extends Advice {
+
+}
---
+++ src/test/java/org/springframework/test/common/WorldServiceBeforeAdvice.java
@@ -0,0 +1,13 @@
+package org.springframework.test.common;
+
+import org.springframework.aop.MethodBeforeAdvice;
+
+import java.lang.reflect.Method;
+
+public class WorldServiceBeforeAdvice implements MethodBeforeAdvice {
+
+	@Override
+	public void before(Method method, Object[] args, Object target) {
+		System.out.println("BeforeAdvice: do something before the earth explodes");
+	}
+}
